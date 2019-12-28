ac57f11d51be24a7ebcd7ae6d872c3af952ccbae
Corey Thompson <corey.dwayne.thompson@gmail.com>
2019-12-28 08:17:11 -0600

    disable export button, add some realistic search values

--- src/SvelteCliSample/ClientApp/src/routes/Build.svelte
+++ src/SvelteCliSample/ClientApp/src/routes/Build.svelte
@@ -10,12 +10,19 @@
     let exportName = "TestList";
     let partNumberInput = "";
     let selectedTests = [];
-    $: isCreateDisabled = ( selectedTests.length < 1 )
-    $: console.log("is create disabled", isCreateDisabled);
+    let selectedStations;
+    let selectedCells;
+    $: isCreateDisabled = ( (selectedTests.length < 1) || 
+    (partNumberInput.length <= 7) || 
+    (selectedCells === undefined) || 
+    (selectedStations === undefined) )
 
+    const optionIdentifier = 'label';
+    const placholderString = 'Search or Browse...'
     const sortSelectedList = ev => { selectedTests = (ev.detail) };
     const handleExport = () => { downloadObjectAsJson(selectedTests, exportName) };
     const groupBy = (item) => item.group;
+    const displayString = (item) => `${item.value} - ${item.label}`;
 
     const stationItems = [
     {value: 'PREP', label: 'Prepare for Production', group: 'Testing'},
@@ -24,18 +31,42 @@
     {value: 'LEAK', label: 'Leak Tester', group: 'Assembly'},
     {value: 'VIZN', label: 'Visual Inspection', group: 'Assembly'},
     {value: 'GLUE', label: 'Automated Glue Machine', group: 'Assembly'},
-    {value: 'BONE', label: 'PCBA Programmer', group: 'Programming'}
+    {value: 'BONE', label: 'PCBA Programmer', group: 'Programming'},
+    {value: 'TINY', label: 'Tiny Tester', group: 'Testing'}
     ];
     
     const cellItems = [
-    {value: '5812', label: 'Skinny Peat / Black Label', group: 'M2'},
-    {value: '5813', label: 'MPC10 / MPC20', group: 'Mid Cost'},
-    {value: '5814', label: 'PV480 / PV485 / HV700', group: 'Low Cost'},
-    {value: '5823', label: 'Furious', group: 'M2'},
-    {value: '5824', label: 'Scallywag', group: 'M2'},
-    {value: '5875', label: 'GPS Puck', group: 'Renesas'},
-    {value: '5830', label: 'LM7', group: 'E2'},
-    {value: '5831', label: 'LM4', group: 'E2'}
+    {value: '5812', label: 'HV1200 (SKINNY PEAT)', group: 'M2'},
+    {value: '5812', label: 'PV1200 (SKINNY PEAT)', group: 'M2'},
+    {value: '5812', label: 'HV1100 (BLACK LABEL)', group: 'M2'},
+    {value: '5812', label: 'PV1100 (BLACK LABEL)', group: 'M2'},
+    {value: '5813', label: 'PV350', group: 'IMX35'},
+    {value: '5813', label: 'PV380', group: 'IMX35'},
+    {value: '5813', label: 'MPC10 (V1)', group: 'TI'},
+    {value: '5813', label: 'MPC10 (V2)', group: 'ST'},
+    {value: '5813', label: 'MPC20 (V1)', group: 'TI'},
+    {value: '5813', label: 'MPC20 (V2)', group: 'ST'},
+    {value: '5813', label: 'CXM110', group: 'ST'},
+    {value: '5814', label: 'HV450', group: 'IMX35'},
+    {value: '5814', label: 'PV480', group: 'IMX35'},
+    {value: '5814', label: 'PV485', group: 'IMX35'},
+    {value: '5814', label: 'HV700', group: 'IMX35'},
+    {value: '5814', label: 'PV780', group: 'IMX35'},
+    {value: '5814', label: 'HV700BP', group: 'M2'},
+    {value: '5814', label: 'PV780B', group: 'M2'},
+    {value: '5821', label: 'FIREBALL', group: 'M2'},
+    {value: '5823', label: 'FURIOUS', group: 'M2'},
+    {value: '5824', label: 'HV750 (SCALLYWAG)', group: 'M2'},
+    {value: '5827', label: 'TV480 (ORION)', group: 'IMX35'},
+    {value: '5828', label: 'PV500 (PATRON)', group: 'G2'},
+    {value: '5830', label: 'GPS PUCK', group: 'H8'},
+    {value: '5831', label: 'LM4', group: 'G2'},
+    {value: '5832', label: 'LM7', group: 'G2'},
+    {value: '5875', label: 'BUTTONPAD', group: 'H8'},
+    {value: '5875', label: 'PDM', group: 'H8'},
+    {value: '5875', label: 'XMD', group: 'H8'},
+    {value: '5809', label: 'PV101', group: 'MONOCOLOR'},
+    {value: '5810', label: 'AQUASTAR', group: 'H8'},
 	];
 
     function addTest(item) {
@@ -71,84 +102,91 @@
 
 </script>
 
-<div>
-    <h1>Test Builder</h1>
-    <form>
-        <div>
-        <label>Part Number</label>
-        <input type="text" bind:value={partNumberInput} placeholder="e.g. 78350777"/>
-        </div>
-        <div class="select">
-        <label>Station Type</label>
-        <Select items={stationItems} {groupBy}></Select>
-        </div>
-        <div class="select">
-        <label>Production Cell</label>
-        <Select items={cellItems} {groupBy}></Select>
-        </div>
-        <div>
-        <label> File Name</label>
-        <input type="text" bind:value={exportName} placeholder="file name"/>
-        </div>
-        <button type="button" class="btn btn-primary" on:click={handleExport} disabled={isCreateDisabled}>Save Test List</button>
-        <small id="emailHelp" class="form-text text-muted">After you select tests, you can download the list for production.</small>
-    </form>
-
-<div class="lists">
-    <div class="tests" ref="tests">
-        <h2>Available Tests</h2>
-        <small class="form-text text-muted section">These are the default tests supported at the moment.<br>Feel free to edit some limits and add them to your current test list.</small>
-
-        {#if $availableTests}
-            {#each $availableTests as item, index}
-            <Test {item} {index} collapsible={true}>
-                <button slot="actionButton" type="button" class="btn btn-warning" on:click={()=> addTest(item)}>
-                Add To Selected Tests
+    <div class="lists">
+        <form>
+            <h1>Test Builder</h1>
+            <div>
+                <label>Part Number</label>
+                <input type="text" bind:value={partNumberInput} placeholder="e.g. 78350777"/>
+            </div>
+            <div class="select">
+                <label>Station Type</label>
+                <Select items={stationItems} bind:selectedValue={selectedStations} placeholder={placholderString} {groupBy}></Select>
+            </div>
+            <div class="select">
+                <label>Production Cell</label>
+                <Select items={cellItems} bind:selectedValue={selectedCells} placeholder={placholderString} {groupBy} {optionIdentifier}></Select>
+            </div>
+            <!-- <div>
+            <label> File Name</label>
+            <input type="text" bind:value={exportName} placeholder="file name"/>
+            </div> -->
+            <div>
+                <button type="button" class="btn" class:btn-primary="{isCreateDisabled}" 
+                class:btn-success="{!isCreateDisabled}" on:click={handleExport} 
+                disabled={isCreateDisabled}>Save Test List
                 </button>
-            </Test>
-            {/each}
-        {:else}
-            <p><em>Loading...</em></p>
-        {/if}
-    </div>
-    <div class="tests">
-        <h2 class="text-warning">Selected Tests</h2>
-        <small class="form-text text-muted section">These are the tests that get saved.<br>Drag them into the order you want to test them!</small>
+                <small id="emailHelp" class="form-text text-muted">After selecting tests, you can download the list.</small>
+            </div>
+        </form>
 
-        {#if selectedTests.length}
-            <SortableList 
-            bind:list={selectedTests} 
-                key="id" 
-            on:sort={sortSelectedList}
-            let:item
-            let:index >
-                <Test {item} {index} collapsible={false}>
-                    <span slot="indexNumber" class="orderIndex">{index+1}</span>
-                    <button slot="actionButton" type="button" class="btn btn-danger" on:click={()=> removeTest(item)}>
-                    Remove From Selected Tests
+        <div class="tests" ref="tests">
+            <h2>Available Tests</h2>
+            <small class="form-text text-muted section">These are the default tests supported at the moment.<br>Feel free to edit some limits and add them to your current test list.</small>
+
+            {#if $availableTests}
+                {#each $availableTests as item, index}
+                <Test {item} {index} collapsible={true}>
+                    <button slot="actionButton" type="button" class="btn btn-warning" on:click={()=> addTest(item)}>
+                    Add To Selected Tests
                     </button>
                 </Test>
-            </SortableList>
-        {:else}
-            <div style="text-align:center; height: 300px; display: flex; align-items: center;">
-            <em style="padding:4rem;">No tests selected</em>
-            </div>
-        {/if}
+                {/each}
+            {:else}
+                <p><em>Loading...</em></p>
+            {/if}
+        </div>
+        <div class="tests">
+            <h2 class="text-warning">Selected Tests</h2>
+            <small class="form-text text-muted section">These are the tests that get saved.<br>Drag them into the order you want to test them!</small>
+
+            {#if selectedTests.length}
+                <SortableList 
+                bind:list={selectedTests} 
+                    key="id" 
+                on:sort={sortSelectedList}
+                let:item
+                let:index >
+                    <Test {item} {index} collapsible={false}>
+                        <span slot="indexNumber" class="orderIndex">{index+1}</span>
+                        <button slot="actionButton" type="button" class="btn btn-danger" on:click={()=> removeTest(item)}>
+                        Remove From Selected Tests
+                        </button>
+                    </Test>
+                </SortableList>
+            {:else}
+                <div style="text-align:center; height: 300px; display: flex; align-items: center;">
+                <em style="padding:4rem;">No tests selected</em>
+                </div>
+            {/if}
+        </div>
     </div>
-</div>
-
-
-</div>
 
 <style>
 form{
     display:inline-block;
     margin-bottom: 4rem;
+    min-width: 200px;
 }
 form div{
     display: flex;
     flex-direction: column;
     padding: 1rem 0;
+}
+form input{
+    height: 42px;
+    padding-left: 1rem;
+    border-width: thin;
 }
 
 .lists{
@@ -163,9 +201,14 @@ form div{
     padding-right: 2rem;
 }
 .select{
-    --borderRadius: default;
+    --borderRadius: 255px 25px 225px 25px/25px 225px 25px 255px;
+    border-top-left-radius: 255px 25px;
+    border-top-right-radius: 25px 225px;
+    border-bottom-right-radius: 225px 25px;
+    border-bottom-left-radius: 25px 255px;
     --placeholderColor: default;
     padding: 1rem 0;
+    border: initial;
 }
 
 .section{
@@ -198,5 +241,9 @@ button:disabled,
 button[disabled]{
   opacity: 0.65; 
   cursor: not-allowed;
+}
+
+.enabled{
+    color: green;
 }
 </style>
